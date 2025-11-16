04346aa8d67c723d709964323108cf0cfdca5739
SimRan <simran@SimRans-MacBook-Air.local>
2025-11-16 22:00:45 +0530

    addded meta data

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+       <Head>
+        <meta name="author" content="Simran" />
+        <meta name="theme-color" content="#ffffff" />
+      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Home from "@/components/homepage";
+import Head from "next/head";
 
 const Homepage = () => {
-  return <Home />;
+  return (
+    <>
+      <Head>
+        <title>Moderen Product Launch</title>
+        <meta name="description" content="This is my website description" />
+        <meta name="keywords" content="nextjs, react, seo" />
+      </Head>
+      <Home />;
+    </>
+  );
 };
 
 export default Homepage;
